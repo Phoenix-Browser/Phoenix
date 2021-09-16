0d7bb4d905ca32e17aa9f024bf30612a8a1ba6dd
Mohammad Moein Abdi <moeinabdi77@gmail.com>
2021-09-16 17:56:10 +0430

    config(dependency-management): #1 Implement Mozilla's component

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,14 +58,65 @@ android {
 
 dependencies {
 
+    //support libs:
     implementation(DependenciesPlugin.Companion.Libs.androidxCore)
     implementation(DependenciesPlugin.Companion.Libs.appCompat)
     implementation(DependenciesPlugin.Companion.Libs.material)
+
+    //compose:
     implementation(DependenciesPlugin.Companion.Libs.Compose.ui)
     implementation(DependenciesPlugin.Companion.Libs.Compose.material)
     implementation(DependenciesPlugin.Companion.Libs.Compose.uiToolingPreview)
-    implementation(DependenciesPlugin.Companion.Libs.lifecycle)
     implementation(DependenciesPlugin.Companion.Libs.Compose.activity)
+
+    //arch components
+    implementation(DependenciesPlugin.Companion.Libs.lifecycle)
+
+    //Mozilla libs:
+
+    //Concept
+    implementation(DependenciesPlugin.Companion.Libs.Mozilla.Concept.awesomeBar)
+    implementation(DependenciesPlugin.Companion.Libs.Mozilla.Concept.engine)
+    implementation(DependenciesPlugin.Companion.Libs.Mozilla.Concept.fetch)
+    implementation(DependenciesPlugin.Companion.Libs.Mozilla.Concept.storage)
+    implementation(DependenciesPlugin.Companion.Libs.Mozilla.Concept.tabsTray)
+
+    //Browser
+    implementation(DependenciesPlugin.Companion.Libs.Mozilla.Browser.awesomeBar)
+    implementation(DependenciesPlugin.Companion.Libs.Mozilla.Browser.engineSystem)
+    //TODO Add engineGecko, But first file a data-review request!
+    implementation(DependenciesPlugin.Companion.Libs.Mozilla.Browser.domains)
+    implementation(DependenciesPlugin.Companion.Libs.Mozilla.Browser.autoComplete)
+    implementation(DependenciesPlugin.Companion.Libs.Mozilla.Browser.errorPages)
+    implementation(DependenciesPlugin.Companion.Libs.Mozilla.Browser.errorPages)
+    implementation(DependenciesPlugin.Companion.Libs.Mozilla.Browser.icons)
+    implementation(DependenciesPlugin.Companion.Libs.Mozilla.Browser.sessionStorage)
+    implementation(DependenciesPlugin.Companion.Libs.Mozilla.Browser.state)
+    implementation(DependenciesPlugin.Companion.Libs.Mozilla.Browser.tabsTray)
+    implementation(DependenciesPlugin.Companion.Libs.Mozilla.Browser.thumbnails)
+
+    //feature
+    implementation(DependenciesPlugin.Companion.Libs.Mozilla.Feature.intent)
+    implementation(DependenciesPlugin.Companion.Libs.Mozilla.Feature.pwa)
+    implementation(DependenciesPlugin.Companion.Libs.Mozilla.Feature.readerView)
+    implementation(DependenciesPlugin.Companion.Libs.Mozilla.Feature.readerView)
+    implementation(DependenciesPlugin.Companion.Libs.Mozilla.Feature.qrCode)
+    implementation(DependenciesPlugin.Companion.Libs.Mozilla.Feature.search)
+    implementation(DependenciesPlugin.Companion.Libs.Mozilla.Feature.session)
+    implementation(DependenciesPlugin.Companion.Libs.Mozilla.Feature.findInPage)
+    implementation(DependenciesPlugin.Companion.Libs.Mozilla.Feature.sitePermissions)
+
+    //Support
+    implementation(DependenciesPlugin.Companion.Libs.Mozilla.Support.ktx)
+    implementation(DependenciesPlugin.Companion.Libs.Mozilla.Support.test)
+    implementation(DependenciesPlugin.Companion.Libs.Mozilla.Support.utils)
+
+    //standalone
+    implementation(DependenciesPlugin.Companion.Libs.Mozilla.Standalone.publicSuffix)
+
+    //End of Mozilla components
+
+    //test libs:
     testImplementation(DependenciesPlugin.Companion.TestLibs.jUnit)
     androidTestImplementation(DependenciesPlugin.Companion.TestLibs.androidJUnitExt)
     androidTestImplementation(DependenciesPlugin.Companion.TestLibs.espressoCore)
