46eb06b2ab8b17a5a86e7b06cd1635942a5114c5
Mohammad Moein Abdi <moeinabdi77@gmail.com>
2021-09-24 16:29:28 +0330

    config(project): fix typo

--- app/src/main/java/phoenix/browser/ui/theme/Theme.kt
+++ app/src/main/java/phoenix/browser/ui/theme/Theme.kt
@@ -28,7 +28,7 @@ private val LightColorPalette = lightColors(
 )
 
 @Composable
-fun PhonixTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+fun PhoenixTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
