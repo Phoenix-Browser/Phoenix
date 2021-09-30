a5674a8cdb59fec08404be9ce636cb1b4c69e07b
Mohammad Moein Abdi <moeinabdi77@gmail.com>
2021-09-30 15:28:46 +0330

    config(project): fix phoenix instrumental test

--- app/src/androidTest/java/phoenix/browser/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/phoenix/browser/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("phonix.browser", appContext.packageName)
+        assertEquals("phoenix.browser", appContext.packageName)
     }
 }
